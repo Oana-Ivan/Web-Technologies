abd47b4b79ad7ad07bdb18a2a4adb5ad59c988b6
Oana-Ivan <49486605+Oana-Ivan@users.noreply.github.com>
2020-02-23 23:32:27 +0200

    Delete produse.js

--- views/html/produse.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-	var text = document.createElement("p");
-	text.innerHTML = "te roooooooooooooooog";
-	var main = document.body.getElementsByTagName("main")[0];
-	main.insertBefore(text,main.firstChild);
-}
